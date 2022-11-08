16d029d72a2a645707bf29c2e1153242944c6f13
SayCheese <cheese0216@gmail.com>
2022-11-08 21:51:38 +0900

    station13 commit try

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,15 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if(e.target.checked){ 
+    console.log("チェックされました。色をつけます");
+    text1.style.backgroundColor = "#ED3833";
+    console.log("色をつけました");
+  } else {
+    text1.style.backgroundColor = "#ffffff";
+    console.log("チェックが外れました");
+  }
 }
+
+// "要素id.checked"の形で、チェックボックスがチェックされた状態を表す。
+// 参考URL: https://webings.net/js/font/
+// backgroundColor; Cが大文字なのに気づくのに時間がかかった。
